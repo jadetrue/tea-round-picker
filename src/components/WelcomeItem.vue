9263e974c72b1061f7368d65d29806a67c90f475
Jade True <jadetruexo@gmail.com>
2022-10-12 14:44:32 +0100

    First commit

---
+++ src/components/WelcomeItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <i>
+      <slot name="icon"></slot>
+    </i>
+    <div>
+      <h3>
+        <slot name="heading"></slot>
+      </h3>
+      <slot></slot>
+    </div>
+  </div>
+</template>
